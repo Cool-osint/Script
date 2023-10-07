8e84df9d2d17bba86a58861ad45d013d40804ead
abhi-s1n <www.abhinav1@gmail.com>
2023-10-07 23:56:08 +0530

    added paypal lookup

--- main.py
+++ main.py
@@ -4,33 +4,34 @@ from tools.num_scripts import *
 from tools.email_scripts import *
 
 banner = """
-888b     d888          888      d8b 888                .d88888b.   .d8888b. 8888888 888b    888 88888888888 
-8888b   d8888          888      Y8P 888               d88P" "Y88b d88P  Y88b  888   8888b   888     888     
-88888b.d88888          888          888               888     888 Y88b.       888   88888b  888     888     
-888Y88888P888  .d88b.  88888b.  888 888  .d88b.       888     888  "Y888b.    888   888Y88b 888     888     
-888 Y888P 888 d88""88b 888 "88b 888 888 d8P  Y8b      888     888     "Y88b.  888   888 Y88b888     888     
-888  Y8P  888 888  888 888  888 888 888 88888888      888     888       "888  888   888  Y88888     888     
-888   "   888 Y88..88P 888 d88P 888 888 Y8b.          Y88b. .d88P Y88b  d88P  888   888   Y8888     888     
-888       888  "Y88P"  88888P"  888 888  "Y8888        "Y88888P"   "Y8888P" 8888888 888    Y888     888     
-                                                                                                            
-                                                                                                                                                                                                              
+888b     d888          888      d8b 888                .d88888b.   .d8888b. 8888888 888b    888 88888888888
+8888b   d8888          888      Y8P 888               d88P" "Y88b d88P  Y88b  888   8888b   888     888
+88888b.d88888          888          888               888     888 Y88b.       888   88888b  888     888
+888Y88888P888  .d88b.  88888b.  888 888  .d88b.       888     888  "Y888b.    888   888Y88b 888     888
+888 Y888P 888 d88""88b 888 "88b 888 888 d8P  Y8b      888     888     "Y88b.  888   888 Y88b888     888
+888  Y8P  888 888  888 888  888 888 888 88888888      888     888       "888  888   888  Y88888     888
+888   "   888 Y88..88P 888 d88P 888 888 Y8b.          Y88b. .d88P Y88b  d88P  888   888   Y8888     888
+888       888  "Y88P"  88888P"  888 888  "Y8888        "Y88888P"   "Y8888P" 8888888 888    Y888     888
+
+
 https://github.com/viralvaghela/OSINT_MOBILE @ViralVaghela
 """
 
- 
+
 services_num = {
     "swiggy": check_swiggy,
     "flipkart": check_flipkart,
     "upstox": check_upstox,
     "instagram": check_instagram,
     "snapdeal": check_snapdeal,
-    "twitter": check_twitter
+    "twitter": check_twitter,
+    "paypal": check_paypal
 }
 
 # Create a dictionary to map service names to functions
 services_email = {
     "twitter":check_twitter,
-    
+
 }
 
 def is_valid_phone_number(identifier):
@@ -39,7 +40,7 @@ def is_valid_phone_number(identifier):
     return re.match(phone_number_pattern, identifier) is not None
 
 def is_valid_email(identifier):
-    # Basic email format validation 
+    # Basic email format validation
     email_pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     return re.match(email_pattern, identifier) is not None
 
